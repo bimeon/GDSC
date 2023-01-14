b1d781e87f22abb939d86622a30c933d68455bdd
3-21AM <117229525+SeohyunLee0321@users.noreply.github.com>
2023-01-14 14:49:29 +0900

    ocr test python idle

--- OCR_FastAPI/ocr_test.py
+++ OCR_FastAPI/ocr_test.py
@@ -1,14 +1,11 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+from PIL import Image
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = R'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+img_eng = Image.open('unnamed_eng.png')
+img_kor = Image.open('unnamed_kor.jpeg')
 
-#print(pytesseract.image_to_string(Image.open('C:/Users/zrowx/OneDrive/바탕 화면/GitHub/GDSC/unnamed.png')))
-print(pytesseract.image_to_string('C:/Users/zrowx/OneDrive/바탕 화면/GitHub/GDSC/unnamed.png'))
+text_eng = pytesseract.image_to_string(img_eng)
+text_kor = pytesseract.image_to_string(img_kor, lang='kor')
 
-#print(pytesseract.get_languages(config=''))
-
-print(pytesseract.image_to_string(Image.open('C:/Users/zrowx/OneDrive/바탕 화면/GitHub/GDSC/unnamed1.png'), lang='kor'))
+print(text_eng)
+print(text_kor)
